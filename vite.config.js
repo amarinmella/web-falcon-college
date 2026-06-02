d9efa23e064b9387f7d8ab63694bd868991b40e0
amarinmella <amarinmella@gmail.com>
2026-06-02 19:19:39 -0400

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/web-falcon-college/',
   build: {
     rollupOptions: {
       input: {
